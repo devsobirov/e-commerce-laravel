1c7bd5e6ad7474649d187c5a06d67ff8a4b6c418
devsobirov <devsobirov@gmail.com>
2022-01-26 16:01:30 +0500

    User Roles migration, user table seeder & role middleware added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\TestMiddleware;
+use App\Http\Middleware\UserRoleMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -65,5 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'game' => TestMiddleware::class,
+        'checkRole' => UserRoleMiddleware::class
     ];
 }
